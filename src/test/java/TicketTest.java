71816a9c134b7c69fab21b86f9f8f9b892e4abd1
anna-almeida <107173243+anna-almeida@users.noreply.github.com>
2022-09-08 18:29:48 +1000

    TicketTest added

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,19 +1,20 @@
-import fit5171.monash.edu.Passenger;
 import fit5171.monash.edu.Person;
+import fit5171.monash.edu.Ticket;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
-@DisplayName("Test class for person")
-public class PersonTest {
-    private Person person;
+@DisplayName("Test for ticket class")
+public class TicketTest {
+    private Ticket ticket;
+
     @BeforeAll
     static void initAll(){
-        PersonTest personTest = new PersonTest();
+        TicketTest ticketTest = new TicketTest();
     }
+
     @BeforeEach
     void init(){
-        person = new Person();
+        ticket = new Ticket();
     }
 }
-
