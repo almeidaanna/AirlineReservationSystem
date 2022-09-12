3fb42ee125da5fd6d2113d2eccd66c9420f5f404
Yaoxing-chen <yche0529@student.monash.edu>
2022-09-12 16:56:56 +1000

    Worked on AirplaneTest.java finish test classes

--- src/test/java/AirplaneTest.java
+++ src/test/java/AirplaneTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+import javax.swing.text.TableView;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -13,58 +16,127 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class AirplaneTest {
 
 
-        private Airplane airplane;
-        @BeforeAll
-        static void initAll(){
-            AirplaneTest airplaneTest = new AirplaneTest();
-        }
-        @BeforeEach
-        void init(){
-            airplane = new Airplane();
-        }
+    private Airplane airplane;
+    @BeforeAll
+    static void initAll(){
+        AirplaneTest airplaneTest = new AirplaneTest();
+    }
+    @BeforeEach
+    void init(){
+        airplane = new Airplane();
+    }
 
-        @Test
-        void testAirplaneValidity()
-        {
-            int airplaneID = 123456;
-            String airplaneModel = "Normal";
-            int businessSitsNumber = 5;
-            int economySitsNumber = 20;
-            int crewSitsNumber = 2;
+    @Test
+    void testAirplaneValidity()
+    {
+        int airplaneID = 123456;
+        String airplaneModel = "AF123";
+        int businessSitsNumber = 5;
+        int economySitsNumber = 20;
+        int crewSitsNumber = 2;
 
 
 
-            int expectedAirplaneID = 123456;
-            String expectedAirplaneModel = "Normal";
-            int expectedBusinessSitsNumber = 5;
-            int expectedEconomySitsNumber = 20;
-            int expectedCrewSitsNumber = 2;
+        int expectedAirplaneID = 123456;
+        String expectedAirplaneModel = "AF123";
+        int expectedBusinessSitsNumber = 5;
+        int expectedEconomySitsNumber = 20;
+        int expectedCrewSitsNumber = 2;
 
+        airplane.setAirplaneID(airplaneID);
+        assertEquals(expectedAirplaneID,airplane.getAirplaneID());
+
+        airplane.setAirplaneModel(airplaneModel);
+        assertEquals(expectedAirplaneModel,airplane.getAirplaneModel());
+
+        airplane.setBusinessSitsNumber(businessSitsNumber);
+        assertEquals(expectedBusinessSitsNumber,airplane.getBusinessSitsNumber());
+
+        airplane.setEconomySitsNumber(economySitsNumber);
+        assertEquals(expectedEconomySitsNumber,airplane.getEconomySitsNumber());
+
+        airplane.setCrewSitsNumber(crewSitsNumber);
+        assertEquals(expectedCrewSitsNumber,airplane.getCrewSitsNumber());
+    }
+
+    @Test
+    void testAirplaneIDEmpty()
+    {
+        int airplaneID = 0;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
             airplane.setAirplaneID(airplaneID);
-            assertEquals(expectedAirplaneID,airplane.getAirplaneID());
+        });
+        assertEquals("AirplaneID can not be Zero or Empty",exception.getMessage());
+    }
 
+    @Test
+    void testAirplaneModelEmpty()
+    {
+        String  airplaneModel= "";
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
             airplane.setAirplaneModel(airplaneModel);
-            assertEquals(expectedAirplaneModel,airplane.getAirplaneModel());
+        });
+        assertEquals("AirplaneModel can not be Empty",exception.getMessage());
+    }
 
+    @Test
+    void testBusinessSitsNumberEmpty()
+    {
+        int businessSitsNumber= 0;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
             airplane.setBusinessSitsNumber(businessSitsNumber);
-            assertEquals(expectedBusinessSitsNumber,airplane.getBusinessSitsNumber());
+        });
+        assertEquals("BusinessSitsNumber can not be Zero or Empty",exception.getMessage());
+    }
 
+    @Test
+    void testEconomySitsNumberEmpty()
+    {
+        int economySitsNumber = 0;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
             airplane.setEconomySitsNumber(economySitsNumber);
-            assertEquals(expectedEconomySitsNumber,airplane.getEconomySitsNumber());
+        });
+        assertEquals("EconomySitsNumber can not be Zero or Empty",exception.getMessage());
+    }
 
-            airplane.setCrewSitsNumber(crewSitsNumber);
-            assertEquals(expectedCrewSitsNumber,airplane.getCrewSitsNumber());
-        }
+    @Test
+    void testCrewSitsNumberEmpty()
+    {
+        int economySitsNumber = 0;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            airplane.setEconomySitsNumber(economySitsNumber);
+        });
+        assertEquals("EconomySitsNumber can not be Zero or Empty",exception.getMessage());
+    }
 
-        @Test
-        void testAirplaneIDEmpty()
-        {
-            int airplaneID = 0;
-            Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
-                airplane.setAirplaneID(airplaneID);
-            });
-            assertEquals("AirplaneID can not be Zero",exception.getMessage());
-        }
+    @Test
+    void testBusinessSitsNumberOverRange()
+    {
+        int businessSitsNumber= (int)((1000000000 - 301 + 1)*Math.random()) + 301;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            airplane.setBusinessSitsNumber(businessSitsNumber);
+        });
+        assertEquals("BusinessSitsNumber should between 1 to 300",exception.getMessage());
+    }
 
+    @Test
+    void testEconomySitsNumberOverRange()
+    {
+        int economySitsNumber= (int)((1000000000 - 301 + 1)*Math.random()) + 301;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            airplane.setBusinessSitsNumber(economySitsNumber);
+        });
+        assertEquals("EconomySitsNumber should between 1 to 300",exception.getMessage());
+    }
+
+    @Test
+    void testCrewSitsNumberOverRange()
+    {
+        int crewSitsNumber= (int)((1000000000 - 301 + 1)*Math.random()) + 301;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            airplane.setBusinessSitsNumber(crewSitsNumber);
+        });
+        assertEquals("CrewSitsNumber should between 1 to 300",exception.getMessage());
+    }
 
 }
