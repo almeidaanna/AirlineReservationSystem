994520591280e8ec0a8c8a54c54e30dbe34e0011
Yaoxing-chen <yche0529@student.monash.edu>
2022-09-13 11:40:14 +1000

    Correct AirplaneTest.java ,working on FlightTest and modified the dependency in Pom

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,22 +1,28 @@
 import fit5171.monash.edu.Airplane;
 import fit5171.monash.edu.Flight;
-import fit5171.monash.edu.FlightCollection;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.sql.Timestamp;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.mockito.Mockito.mock;
 
 
 @DisplayName("This is test class for Flight")
+@ExtendWith(MockitoExtension.class)
 public class FlightTest {
 
     private Flight flight;
+
+    @Mock
+    private Airplane airplane;
 
     @BeforeAll
     static void initAll(){
@@ -25,6 +31,7 @@ public class FlightTest {
 
     @BeforeEach
     void init(){
+
         flight = new Flight();
     }
 
@@ -32,35 +39,96 @@ public class FlightTest {
     void testFlightValidity()
     {
         int flightID = 123456;
-        String airplaneModel = "AF123";
         String departTo = "Melbourne" ;
         String departFrom =  "Sydney";
         String code = "123456";
-        String company = "AUsAir";
+        String company = "AUAirLine";
         Timestamp dateFrom = Timestamp.valueOf("2022/09/10");
         Timestamp dateTo = Timestamp.valueOf("2022/09/11");
+        airplane = mock(Airplane.class);
 
-        int expectedAirplaneID = 123456;
-        String expectedAirplaneModel = "AF123";
-        int expectedBusinessSitsNumber = 5;
-        int expectedEconomySitsNumber = 20;
-        int expectedCrewSitsNumber = 2;
+        int expectedFlightID = 123456;
+        String expectedDepartTo = "Melbourne" ;
+        String expectedDepartFrom =  "Sydney";
+        String expectedCode = "123456";
+        String expectedCompany = "AUAirLine";
+        Timestamp expectedDateFrom = Timestamp.valueOf("2022/09/10");
+        Timestamp expectedDateTo = Timestamp.valueOf("2022/09/11");
 
-//        airplane.setAirplaneID(airplaneID);
-//        assertEquals(expectedAirplaneID,airplane.getAirplaneID());
-//
-//        airplane.setAirplaneModel(airplaneModel);
-//        assertEquals(expectedAirplaneModel,airplane.getAirplaneModel());
-//
-//        airplane.setBusinessSitsNumber(businessSitsNumber);
-//        assertEquals(expectedBusinessSitsNumber,airplane.getBusinessSitsNumber());
-//
-//        airplane.setEconomySitsNumber(economySitsNumber);
-//        assertEquals(expectedEconomySitsNumber,airplane.getEconomySitsNumber());
-//
-//        airplane.setCrewSitsNumber(crewSitsNumber);
-//        assertEquals(expectedCrewSitsNumber,airplane.getCrewSitsNumber());
+
+        flight.setFlightID(flightID);
+        assertEquals(expectedFlightID,flight.getFlightID());
+
+        flight.setDepartTo(departTo);
+        assertEquals(expectedDepartTo,flight.getDepartTo());
+
+        flight.setDepartFrom(departFrom);
+        assertEquals(expectedDepartFrom,flight.getDepartFrom());
+
+        flight.setCode(code);
+        assertEquals(expectedCode,flight.getCode());
+
+        flight.setCompany(company);
+        assertEquals(expectedCompany,flight.getCompany());
+
+        flight.setDateTo(dateTo);
+        assertEquals(expectedDateTo,flight.getDateTo());
+
+        flight.setDateFrom(dateFrom);
+        assertEquals(expectedDateFrom,flight.getDateFrom());
+        // Just mock a class of Airplane, when flight.getAirplane = mocked class, then it's work
+        flight.setAirplane(airplane);
+        assertEquals(airplane,flight.getAirplane());
+
     }
 
+    @Test
+    void testFlightIDEmpty()
+    {
+        int flightID = 0;
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            flight.setFlightID(flightID);
+        });
+        assertEquals("FlightID can not be Zero or Empty",exception.getMessage());
+    }
 
+    @Test
+    void testDepartToEmpty()
+    {
+        String departTo = "";
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            flight.setDepartTo(departTo);
+        });
+        assertEquals("DepartTo can not be Empty",exception.getMessage());
+    }
+
+    @Test
+    void testDepartFromEmpty()
+    {
+        String departFrom = "";
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            flight.setDepartFrom(departFrom);
+        });
+        assertEquals("DepartFrom can not be Empty",exception.getMessage());
+    }
+
+    @Test
+    void testCodeEmpty()
+    {
+        String code = "";
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            flight.setCode(code);
+        });
+        assertEquals("Code can not be Empty",exception.getMessage());
+    }
+
+    @Test
+    void testCompanyEmpty()
+    {
+        String code = "";
+        Throwable exception = assertThrows(java.lang.IllegalArgumentException.class, () -> {
+            flight.setCode(code);
+        });
+        assertEquals("Code can not be Empty",exception.getMessage());
+    }
 }
